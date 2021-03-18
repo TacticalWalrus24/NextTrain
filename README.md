632cd4bb29ad9b9f02efe065aa009a44d8189024
DesertSnek <jamesbaker2201@gmail.com>
2021-03-18 19:48:10 +1100

    - Fixed comments - added one form of input validation for one function - added contributions to readme (I think its up to date)

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NextTrain
 # By James Baker (6074) & Vaughan Bunt (7120)
+
+# Vaughan Bunt - Lead developer, 'GenerateTimeTable' function, Developed SQL database, Most other SQL related stuff
+
+# James Baker - Project lead, 'Retrieve line' function, AWS components (later), 
--- advanced studio2/TimeTableGen.py
+++ advanced studio2/TimeTableGen.py
@@ -2,6 +2,7 @@ import pymysql
 import csv
 # database connection
 connection = pymysql.connect(host="localhost", user="root", passwd="", database="nexttrain")
+# Establish cursor
 cursor = connection.cursor()
 
 def main():
@@ -10,7 +11,7 @@ def main():
     # executing the quires
     cursor.execute(retrive)
     rows = cursor.fetchall()
-    #if the quirey returns a result get the lines
+    # if the quirey returns a result get the lines
     if rows != None:
         RetriveLine(rows)
 
@@ -18,46 +19,67 @@ def RetriveLine(rows):
     print("Available Lines:\n")
     count = 0
     lines = []
-    #for each of the returned results
+    # for each of the returned results
     for row in rows:
         count += 1
-        #add line objects to a list
+        # add line objects to a list
         lines.append(Line(row[1], row[0]))
-        #provide user with a selection list
+        # provide user with a selection list
         print(f"{count}\t{row[1]}")
-    #ask user for input
+    # ask user for input
     val = input("Select Line (by number or 'all'): " )
     selection = 0
-    if val != "all":
-        selection = int(val)
+    # If user Imput was a specific line
+    # NOTE TO SELF: Need to add input validation at some point
+    if val.lower() != "all":
+        # set selection to user imput value
+        selection = int(val
+        # get the requested line
         l = lines[selection - 1]
+        # user imputs the departure time
         val = input(f"{l.name} departure Time(eg 13:30): ")
+        # split the hours from the minutes
         hours = val.split(":")
+        # Convert the hours into minutes and add them to minutes already there
         time = (60 * int(hours[0]) + int(hours[1]))
+        # generate the timetable with changes
         GenerateTimeTable(time, l)
+    # if user imput was 'all'
     else:
         for l in lines:
+            # user imputs the departure time
             val = input(f"{l.name} departure Time(eg 13:30): ")
+            # split the hours from the minutes
             hours = val.split(":")
+            # Convert the hours into minutes and add them to minutes already there
             time = (60 * int(hours[0]) + int(hours[1]))
+            # generate the timetable with changes
             GenerateTimeTable(time, l)
 
 def GenerateTimeTable(startTime, line):
+    # define time as the startime
     time = startTime
+    # retrive is set as the stations from inputted line
     retrive = f"SELECT StationID FROM stationlines WHERE LineID = '{line.id}' ORDER BY StopNumber"
+    # run the retrive function
     cursor.execute(retrive)
     rows = [x[0] for x in cursor.fetchall()]
     stations = []
+    # for each row
     for row in rows:
+        # get the station
         retrive = f"SELECT StationName, StationID FROM stations WHERE StationID = '{row}'"
         cursor.execute(retrive)
         stationRows = cursor.fetchall()
+        # append to the station
         for sRow in stationRows:
             stations.append(Station(sRow[0], sRow[1]))
+    # get the railID
     retrive = f"SELECT RailID FROM linesegments WHERE LineID = '{line.id}'"
     cursor.execute(retrive)
     rows = [x[0] for x in cursor.fetchall()]
     rails = []
+    # for each row print rows data, then append the row
     for row in rows:
         retrive = f"SELECT * FROM rails WHERE RailID = '{row}'"
         cursor.execute(retrive)
@@ -67,6 +89,7 @@ def GenerateTimeTable(startTime, line):
             rails.append(Rail(rR[0], rR[1], rR[2], rR[3], rR[4]))
     prior = ""
     timeTable = []
+    # for each station, print the inforation for the station, set the last station as current staiton, write to the CSV file.
     for s in stations:
         if prior != "":
             for r in rails:
@@ -74,12 +97,16 @@ def GenerateTimeTable(startTime, line):
                 if (r.startStation == prior and r.endStation == s.id) or (r.startStation == s.id and r.endStation == prior):
                     print(r.length)
                     time += int(r.length)
+        # update the timetable
         timeTable += [[f"{s.name}, {time}"]]
+        # set the prior station
         prior = s.id
+    # write to csv
     with open(f'{line.name}.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(timeTable)
 
+# defines of the classes for SQL
 class Line:
     def __init__(self, name, id):
         self.name = name
